7f0adb8542337e2bf69df0a0e2462ad26c70f15f
Rguerrero19 <sas.ramonmendoza@gmail.com>
2025-11-20 14:01:43 -0600

    Topic of study: SQL queries and subqueries

--- consulta_subconsulta.sql
+++ consulta_subconsulta.sql
@@ -12,8 +12,32 @@ SELECT nombre_empleado, nombre_departamento
 FROM empleados 
 INNER JOIN departamento ON ID_departamento = ID_departamento
 
-LEFT JOIN:
+LEFT JOIN: Devuelve todas las filas de la tabla de la fila izquierda (primera tabla mencionada
+en la consulta) y las filas considentes de la tabla de la derecha (segunda tabla)
 
-RIGHT JOIN
+SELECT nombre_empleado, nombre_departamento 
+FROM empleados 
+LEFT JOIN departamento ON ID_departamento = ID_departamento
 
-FULL OUTER JOIN
+RIGHT JOIN: Funciona de manera inversa al LEFT JOIN, retornando todas las filas de la tabla
+derecha y las filas considentes de la tabla izquierda
+
+SELECT nombre_empleado, nombre_departamento 
+FROM empleados 
+RIGHT JOIN departamento ON ID_departamento = ID_departamento
+
+FULL OUTER JOIN: combina LEFT JOIN y RIGHT JOIN, devolviendo filas cuando hay conisedencia
+en una de las tablas. Si no hay considencia retornara valor NULL de le lado sin correspondencia.
+Cabe mencionar que SQL no soporta esta consulta de manera directa pero si se puede realizar una
+simulacion utilizando LEFT JOIN y RIGHT JOIN agregando la consulta UNION
+
+
+SELECT nombre_empleado, nombre_departamento 
+FROM empleados 
+LEFT JOIN departamento ON ID_departamento = ID_departamento
+
+UNION
+
+SELECT nombre_empleado, nombre_departamento 
+FROM empleados 
+RIGHT JOIN departamento ON ID_departamento = ID_departamento
