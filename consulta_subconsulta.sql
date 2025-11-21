47cb55329f18270db1f34486c435e81634effc84
Rguerrero19 <sas.ramonmendoza@gmail.com>
2025-11-21 13:21:54 -0600

    Topic update and completion

--- consulta_subconsulta.sql
+++ consulta_subconsulta.sql
@@ -11,20 +11,21 @@ INNER JOIN: Se utiliza para retornar filas cuando haya almenos una coinsidenca e
 SELECT nombre_empleado, nombre_departamento 
 FROM empleados 
 INNER JOIN departamento ON ID_departamento = ID_departamento
-
+________________________________________________________________________________________________
 LEFT JOIN: Devuelve todas las filas de la tabla de la fila izquierda (primera tabla mencionada
 en la consulta) y las filas considentes de la tabla de la derecha (segunda tabla)
 
 SELECT nombre_empleado, nombre_departamento 
 FROM empleados 
 LEFT JOIN departamento ON ID_departamento = ID_departamento
-
+________________________________________________________________________________________________
 RIGHT JOIN: Funciona de manera inversa al LEFT JOIN, retornando todas las filas de la tabla
 derecha y las filas considentes de la tabla izquierda
 
 SELECT nombre_empleado, nombre_departamento 
 FROM empleados 
 RIGHT JOIN departamento ON ID_departamento = ID_departamento
+________________________________________________________________________________________________
 
 FULL OUTER JOIN: combina LEFT JOIN y RIGHT JOIN, devolviendo filas cuando hay conisedencia
 en una de las tablas. Si no hay considencia retornara valor NULL de le lado sin correspondencia.
