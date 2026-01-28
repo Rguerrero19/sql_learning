3458c7ab7618de55084ef25d3ac1432012795654
Rguerrero19 <sas.ramonmendoza@gmail.com>
2026-01-28 14:13:10 -0600

    ipdate 28.01.26 crea table ventas/compras

--- exercices_270126.sql
+++ exercices_270126.sql
@@ -1,10 +1,20 @@
+                CREACION DE TABLAS NECESARIAS
+    CREATE TABLE empleados
+    (ID_empleado INT(5) PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    apellido VARCHAR(20) NOT NULL,
+    fecha_contrato DATE NOT NULL);
+
+    ALTER TABLE compras ADD CONSTRAINT empleado_compra FOREIGN KEY (ID_empleado) REFERENCES empleados(ID_empleado)
+
+
                     INSERTS
 
 Inserta 3 empleados nuevos.
     INSERT INTO empleados VALUES
     (1001,'Ignacio','Mendoza','2026-01-27'),
-    (1002,'Daniela','Flores','2026-01-27')'
-    (1003,'Norma','Silvina','2026-01-27);
+    (1002,'Daniela','Flores','2026-01-27'),
+    (1003,'Norma','Silvina','2026-01-27');
 
 Inserta 5 productos en distintas categorías.
     INSERT INTO productos VALUES
@@ -13,3 +23,19 @@ Inserta 5 productos en distintas categorías.
     (750123456,'pan dulce','frescos',12),
     (750555111,'leche monarca 1.8L','lacteos',12),
     (750333555,'vino tinto cavernet','vinos y licores',12);
+
+Inserta 2 proveedores.
+    INSERT INTO proveedores VALUES
+    (000456,'sabritas','carlos','5551234567'),
+    (001782,'coca cola','abraham','5557654321');
+
+creacion de ralaciones (llaves foraneas)
+    ALTER TABLE ventas
+    ADD CONSTRAINT ventas_empleado
+    FOREIGN KEY (ID_empleado) 
+    REFERENCES empleados(ID_empleado);
+
+    ALTER TABLE compras 
+    ADD CONSTRAINT empleado_compra 
+    FOREIGN KEY (ID_empleado) 
+    REFERENCES empleados(ID_empleado);
