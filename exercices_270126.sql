e9d43887e362d6fe6e9203bb5683de381c16e2ce
Rguerrero19 <sas.ramonmendoza@gmail.com>
2026-01-27 21:15:56 -0600

    exercises completed on the day 27/01/2026

---
+++ exercices_270126.sql
@@ -0,0 +1,15 @@
+                    INSERTS
+
+Inserta 3 empleados nuevos.
+    INSERT INTO empleados VALUES
+    (1001,'Ignacio','Mendoza','2026-01-27'),
+    (1002,'Daniela','Flores','2026-01-27')'
+    (1003,'Norma','Silvina','2026-01-27);
+
+Inserta 5 productos en distintas categorías.
+    INSERT INTO productos VALUES
+    (750767676,'sidral mundet 2L','refrescos familiares',12),
+    (750232323,'sabritas original 50g','frituras',12),
+    (750123456,'pan dulce','frescos',12),
+    (750555111,'leche monarca 1.8L','lacteos',12),
+    (750333555,'vino tinto cavernet','vinos y licores',12);
