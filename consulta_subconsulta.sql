8962430a2dbc27abaa8ac3e8750d6dc74c39fd6a
Rguerrero19 <sas.ramonmendoza@gmail.com>
2026-02-16 11:09:25 -0600

    The query definition and syntax were added SELECT DISTINCT

--- consulta_subconsulta.sql
+++ consulta_subconsulta.sql
@@ -42,3 +42,38 @@ UNION
 SELECT nombre_empleado, nombre_departamento 
 FROM empleados 
 RIGHT JOIN departamento ON ID_departamento = ID_departamento
+
+JOIN tambien se puede utilizar conlas subconsultas:
+COUNT:contar los valores en una tabla .
+AVG:promedio de los varores dentro de una tabla.
+MAX:valor mas alto de una tabla.
+MIN:valor minimo de una tabla.
+
+DISTINCT:Crea una consulta SQL que enlista los valores de una o mas tablas
+tablas sin mostrar valores repetidos.
+
+------------------SYNTAXIS--------------------------
+SELECT DISTINCT columna
+FROM tabla
+# esta consulta mostrara los valores distintos de una tabla, se peude
+agregar mas de una columna a consultar
+
+SELECT DISTINCT tabla_1.columna_1
+FROM tabla_1
+INNER JOIN tabla_2 ON tabla_1.PRIMARY_KEY = tabla_2.FOREIGN_KEY;
+# esta consulta mostrara los valores de las columnas seleccionadas
+existentes en dos tablas sin repetir valores uniendolas por sus llaves
+primaria de la tablla_1 con la llave foranea de la tabla_2
+
+-----------------------EJEMPLO--------------------------
+SELECT DISTINCT productos.nombre_producto
+FROM productos
+INNER JOIN ventas ON productos.id_producto = ventas.id_producto;
+#esta consulta mostrara el nombre e id de los productos que conciden
+en las tablas ventas y en productos (que se han vendido) sin motrar
+valores repedidos.
+la traduccion podria ser:   SELECCIONA DIFERENCIAS de tabla_1.valor
+                            DE la tabla_1
+                            JUNTANDO ventas CON tabla_1.prymary_key = tabla_2.foreign_key
+
+#se pueden agregar los valores de cinsulta necesrios en SELECT DISTINCT
